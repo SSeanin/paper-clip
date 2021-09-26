7d20df8270fc09098e954b847f8e5e18880f6128
Saeed Andalib <saeedandalib5@gmail.com>
2021-09-26 12:00:06 +0330

    Edited static file naming convention

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,39 @@
 const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
-    mode: "development",
-    entry: ['./src/index.js'],
-    output: {
-        path: `${path.resolve(__dirname, 'dist')}`,
-        assetModuleFilename: "images/[hash][name][ext]"
-    },
-    module: {
-        rules: [
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    process.env.NODE_ENV === 'development' ? 'style-loader' : MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'sass-loader'
-                ]
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-            },
-            {
-                test: /.(htm|html)$/i,
-                use: ['html-loader'],
-            }
+  mode: "development",
+  entry: ['./src/index.js'],
+  output: {
+    path: `${path.resolve(__dirname, 'dist')}`,
+    assetModuleFilename: "images/[hash][ext]"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          process.env.NODE_ENV === 'development' ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
         ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/pages/index.html",
-            filename: "index.html"
-        }),
-        new MiniCssExtractPlugin()
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /.(htm|html)$/i,
+        use: ['html-loader'],
+      }
     ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/pages/index.html",
+      filename: "index.html"
+    }),
+    new MiniCssExtractPlugin()
+  ]
 }
